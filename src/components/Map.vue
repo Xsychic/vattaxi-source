71ad44b0e0f19b46d115d6873d0ebf9453012475
adam <ajb042@student.bham.ac.uk>
2022-11-23 14:28:00 +0000

    Add map filter panel

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,22 +1,68 @@
 <script setup>
+    import { ref } from 'vue';
 
+    const showFilters = ref(false);
 </script>
 
 <template>
     <div class='chart'>
         <div class='controls'>
-            <div class='control-button filter'>
-                <font-awesome-icon icon='fa-solid fa-filter'></font-awesome-icon>
-            </div>  
-            
-            <div class='zoom'>
-                <div class='control-button zoom-in'>
-                    <font-awesome-icon icon='fa-solid fa-plus'></font-awesome-icon>
-                </div>
-                <div class='control-button zoom-out'>
-                    <font-awesome-icon icon='fa-solid fa-minus'></font-awesome-icon>
+            <div class='buttons'>
+                <div class='control-button filter' @click='showFilters = !showFilters'>
+                    <font-awesome-icon icon='fa-solid fa-filter'></font-awesome-icon>
+                </div>  
                 
+                <div class='zoom'>
+                    <div class='control-button zoom-in'>
+                        <font-awesome-icon icon='fa-solid fa-plus'></font-awesome-icon>
+                    </div>
+                    <div class='control-button zoom-out'>
+                        <font-awesome-icon icon='fa-solid fa-minus'></font-awesome-icon>
+                    
+                    </div>
                 </div>
+            </div>
+
+            <div class='filters' v-show='showFilters'>
+                <h5 class='filter-header'>Markings</h5>
+                <ul class='filter-list'>
+                    <li>
+                        <input class='filter-checkbox' name='runway-markings' type='checkbox' checked>
+                        Runway
+                    </li>
+                    <li>
+                        <input class='filter-checkbox' name='taxi-markings' type='checkbox' checked>
+                        Taxi
+                    </li>
+                    <li>
+                        <input class='filter-checkbox' name='hp-markings' type='checkbox' checked>
+                        Holding Point
+                    </li>
+                    <li>
+                        <input class='filter-checkbox' name='stand-markings' type='checkbox' checked>
+                        Stand
+                    </li>
+                </ul>
+
+                <h5 class='filter-header'>Labels</h5>
+                <ul class='filter-list'>
+                    <li>
+                        <input class='filter-checkbox' name='taxi-labels' type='checkbox' checked>
+                        Taxi
+                    </li>
+                    <li>
+                        <input class='filter-checkbox' name='hp-labels' type='checkbox' checked>
+                        Holding Point
+                    </li>
+                    <li>
+                        <input class='filter-checkbox' name='stand-labels' type='checkbox' checked>
+                        Stand
+                    </li>
+                    <li>
+                        <input class='filter-checkbox' name='building-labels' type='checkbox' checked>
+                        Building
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -36,6 +82,9 @@
 
     .controls {
         position: absolute;
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: flex-end;
         bottom: 15px;
         right: 15px;
     }
@@ -49,6 +98,8 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        user-select: none;
+        -webkit-user-select: none;
     }
 
     .control-button:active, .control-button:focus, .control-button:hover {
@@ -59,6 +110,46 @@
     .filter {
         margin-bottom: 10px;
         font-size: 22px;
+    }
+
+    .filter-checkbox {
+        position: relative;
+        top: 3px;
+        margin: 0;
+        transform: scale(0.85);
+    }
+
+    .filter-header {
+        margin: 0;
+        margin-bottom: 1px;
+        font-size: 13px;
+    }
+
+    .filter-header:nth-of-type(2) {
+        margin-top: 4px;
+    }
+
+    .filter-list {
+        list-style-type: none;
+        padding: 0;
+        font-size: 13px;
+        margin: 0;
+    }
+
+    .filter-list li {
+        margin-top: -2px;
+    }
+
+    .filters {
+        background: var(--dark-grey);
+        margin-right: 10px;
+        border-radius: 10px;
+        padding: 6px 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        font-size: 15px;
+        width: 110px;
     }
 
     .zoom {
